6ffb7804d562f7ce1de97ac4d078b04e6a9e6ab1
prliu <2920250+prliu@users.noreply.github.com>
2019-04-17 01:44:14 +0800

    add 'semantic-ui-react' to dependency.

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+import { Button } from "semantic-ui-react";
+
 import "./styles.css";
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <Button primary>OK</Button>
     </div>
   );
 }
