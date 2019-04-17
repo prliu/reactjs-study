7f4c602b230dc5c43b5bec16da1743997162e929
prliu <2920250+prliu@users.noreply.github.com>
2019-04-17 12:48:39 +0800

    add some testing for an AJAX call.

--- src/query-form.js
+++ src/query-form.js
@@ -1,5 +1,5 @@
 import React from "react";
-
+import axios from "axios";
 import { Form, Icon } from "semantic-ui-react";
 
 const options = [
@@ -9,6 +9,19 @@ const options = [
 ];
 
 class QueryForm extends React.Component {
+  sendQuery = () => {
+    axios
+      .get("https://27effcad.ngrok.io/brokers/")
+      .then(function(response) {
+        // handle success
+        console.log(response);
+      })
+      .catch(function(error) {
+        // handle error
+        console.log(error);
+      });
+  };
+
   render() {
     return (
       <Form>
@@ -23,7 +36,7 @@ class QueryForm extends React.Component {
           />
         </Form.Group>
         <Form.Group inline>
-          <Form.Button primary>
+          <Form.Button primary onClick={this.sendQuery}>
             <Icon name="play circle" />
             Query
           </Form.Button>
