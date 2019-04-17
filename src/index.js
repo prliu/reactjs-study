f31c2eab595b97cc68260c7fe87c71459323de0b
prliu <2920250+prliu@users.noreply.github.com>
2019-04-18 00:44:55 +0800

    create a query form which can be hide.

--- src/index.js
+++ src/index.js
@@ -2,14 +2,13 @@ import React, { Fragment } from "react";
 import ReactDOM from "react-dom";
 
 import {
-  Button,
   Container,
   Divider,
   Header,
   Message,
-  Form,
-  Field,
-  Label
+  Segment,
+  Button,
+  Icon
 } from "semantic-ui-react";
 
 import "./styles.css";
@@ -17,21 +16,41 @@ import "./styles.css";
 import QueryForm from "./query-form";
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { isClicked: true, sid: 6176, sname: "瑞儀" };
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick = () => {
+    this.setState(state => ({
+      isClicked: !state.isClicked
+    }));
+  };
+
   render() {
+    var obj = this.state.isClicked ? <QueryForm visible={true} /> : null;
+
     return (
       <Container>
         <Divider hidden />
-        <Header as="h1" dividing>
-          Major Weight of Stocks
-        </Header>
+        <Message>
+          <Message.Header>
+            {this.state.sname} ({this.state.sid})
+            <Button icon floated="right" size="mini" onClick={this.handleClick}>
+              {this.state.isClicked ? (
+                <Icon name="angle double down" />
+              ) : (
+                <Icon name="angle double up" />
+              )}
+            </Button>
+          </Message.Header>
 
-        <QueryForm />
+          <p>This is a simple message</p>
+        </Message>
 
-        <Message
-          header="TODO"
-          content="How about the start and end date??"
-          warning
-        />
+        {obj}
       </Container>
     );
   }
--- src/query-form.js
+++ src/query-form.js
@@ -1,6 +1,10 @@
 import React from "react";
 import axios from "axios";
-import { Form, Icon } from "semantic-ui-react";
+import { Form, Icon, Segment } from "semantic-ui-react";
+
+import "react-dates/initialize";
+import "react-dates/lib/css/_datepicker.css";
+import { DateRangePicker, SingleDatePicker } from "react-dates";
 
 const options = [
   { key: "D", text: "Daily", value: "D" },
@@ -9,6 +13,12 @@ const options = [
 ];
 
 class QueryForm extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { visible: props.visible };
+  }
+
   sendQuery = () => {
     axios
       .get("https://27effcad.ngrok.io/brokers/")
@@ -23,31 +33,45 @@ class QueryForm extends React.Component {
   };
 
   render() {
-    return (
-      <Form>
-        <Form.Group widths="equal">
-          <Form.Input fluid label="Stock Id" placeholder="Stock Id" />
-          <Form.Select
-            fluid
-            label="Period"
-            selectedLabel="W"
-            options={options}
-            placeholder="Period"
-          />
-        </Form.Group>
-        <Form.Group inline>
-          <Form.Button primary onClick={this.sendQuery}>
-            <Icon name="play circle" />
-            Query
-          </Form.Button>
-          <Form.Button>Clear</Form.Button>
-          <Form.Button>
-            <Icon name="calculator" />
-            Calculate Period
-          </Form.Button>
-        </Form.Group>
-      </Form>
-    );
+    if (!this.state.visible) {
+      return null;
+    } else
+      return (
+        <Segment floated="right">
+          <Form>
+            <Form.Group widths="equal">
+              <Form.Input label="Stock Id" placeholder="Stock Id" />
+              <Form.Select
+                label="Period"
+                selectedLabel="W"
+                options={options}
+                placeholder="Period"
+              />
+            </Form.Group>
+            <Form.Group widths="equal">
+              <Form.Field>
+                <label>Start Date</label>
+                <SingleDatePicker />
+              </Form.Field>
+              <Form.Field>
+                <label>End Date</label>
+                <SingleDatePicker />
+              </Form.Field>
+            </Form.Group>
+            <Form.Group inline>
+              <Form.Button primary onClick={this.sendQuery}>
+                <Icon name="play circle" />
+                Query
+              </Form.Button>
+              <Form.Button>Clear</Form.Button>
+              <Form.Button right floated>
+                <Icon name="calculator" />
+                Calculate Period
+              </Form.Button>
+            </Form.Group>
+          </Form>
+        </Segment>
+      );
   }
 }
 
